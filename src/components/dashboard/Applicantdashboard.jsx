04629ee459e2b4bfaaca637cbda9745868e430c6
Neo <jimmyyaocs@gmail.com>
2023-05-04 22:43:50 +1000

    style: re-style to match the UI desgin

--- src/components/dashboard/Applicantdashboard.jsx
+++ src/components/dashboard/Applicantdashboard.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import supabase from '@/lib/supabaseClient'
+import { GrDocumentUpload } from 'react-icons/gr'
+import { FiDownload } from 'react-icons/fi'
 import { useSession } from 'next-auth/react'
+
 const Applicantdashboard = () => {
   const [name, setName] = useState('')
   const [phone, setPhone] = useState('')
@@ -55,53 +58,68 @@ const Applicantdashboard = () => {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full max-w-md  p-4 sm:p-8 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-semibold mb-6">Application Form</h2>
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="w-full max-w-md  p-4 sm:p-8 bg-white rounded-lg shadow-md ">
+        <h2 className="text-2xl font-semibold mb-6 text-dark">Application Form</h2>
         <input
-          className="w-full p-2 mb-4 border rounded"
+          className="w-full p-2 mb-4 border-[3px] border-dark rounded"
           type="text"
           placeholder="Name"
           value={name}
           onChange={nameHandler}
         />
         <input
-          className="w-full p-2 mb-4 border rounded"
-          type="tel"
+          className="w-full p-2 mb-4 border-[3px] border-dark rounded"
+          type="text"
           placeholder="Phone"
           value={phone}
           onChange={phoneHandler}
         />
         <input
-          className="w-full p-2 mb-6 border rounded"
+          className="w-full p-2 mb-6 border-[3px] border-dark rounded"
           type="email"
           placeholder="Email"
           value={email}
           onChange={emailHandler}
         />
+
+        <label
+          htmlFor="file_input"
+          className="h-[100px] w-[160px] border-[3px] border-dark rounded cursor-pointer flex flex-col items-center justify-center"
+        >
+          {resumeFile ? (
+            <span className="text-l">Update Resume</span>
+          ) : (
+            <span className="text-l">Upload Resume</span>
+          )}
+          <GrDocumentUpload className="text-5xl mt-2" />
+        </label>
+        <input
+          type="file"
+          accept="application/pdf"
+          id="file_input"
+          onChange={resumeHandler}
+          className="hidden"
+        />
         {resumePreviewUrl && (
-          <div className="border rounded p-2 mb-6">
-            <a href={resumePreviewUrl} target="_blank" rel="noopener noreferrer" className="">
-              <span>Preview Resume:</span>
-              {resumeFile.name}
+          <div className="border-[3px] border-dark rounded p-2 mt-6">
+            <a
+              href={resumePreviewUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex justify-between"
+            >
+              <div>
+                <span>Uploaded Resume: </span>
+                {resumeFile.name}
+              </div>
+
+              <FiDownload className="text-2xl inline-block" />
             </a>
           </div>
         )}
-        <label
-          htmlFor="file_input"
-          className="w-1/2 p-2  border rounded cursor-pointer flex items-center justify-center"
-        >
-          {resumeFile ? <span>Update Resume</span> : <span>Upload Resume</span>}
-          <input
-            type="file"
-            accept="application/pdf"
-            id="file_input"
-            onChange={resumeHandler}
-            className="hidden"
-          />
-        </label>
         <button
-          className="w-full px-4 py-2 font-semibold text-black bg-indigo-200 rounded mt-6"
+          className="w-full px-4 py-2 font-semibold text-black bg-primary text-white rounded mt-6"
           onClick={handleSubmit}
         >
           Submit
