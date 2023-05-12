b47cd51c47a9fff1739972c9c764635a35fc8fa0
Jimmy Yao <jimmyyaocs@gmail.com>
2023-05-12 19:44:53 +1000

    refactory: refining the logic for creating and updating profiles

--- src/components/dashboard/Applicantdashboard.jsx
+++ src/components/dashboard/Applicantdashboard.jsx
@@ -5,25 +5,25 @@ import { useSession } from 'next-auth/react'
 import { createSupabaseClient } from '@/lib/supabaseClient'
 
 const Applicantdashboard = ({ applicant, onRefresh }) => {
-  console.log('applicant: ', applicant)
   const [name, setName] = useState('')
   const [phone, setPhone] = useState('')
   const [email, setEmail] = useState('')
-  const [resumeFile, setResumeFile] = useState(null)
+  const [resumeFile, setResumeFile] = useState('')
   const [resumePreviewUrl, setResumePreviewUrl] = useState('')
 
+  // set applicant data to state
   useEffect(() => {
     if (applicant) {
-      setName(applicant.name || '')
+      setName(applicant.name)
       setPhone(applicant.phone)
       setEmail(applicant.email)
     }
   }, [applicant])
 
   const { data: session, status } = useSession()
-  console.log('session: ', session)
 
   const supabase = createSupabaseClient(session.supabaseAccessToken)
+
   const nameHandler = (e) => {
     setName(e.target.value)
   }
@@ -37,23 +37,12 @@ const Applicantdashboard = ({ applicant, onRefresh }) => {
   }
 
   const resumeHandler = (e) => {
+    e.preventDefault()
     const file = e.target.files[0]
     setResumeFile(file)
-    setResumePreviewUrl(URL.createObjectURL(file))
-  }
-
-  const ifApplicantExists = async () => {
-    const { data, error } = await supabase
-      .from('applicants')
-      .select()
-      .limit(1)
-      .single()
-      .eq('users_id', session.user.id)
-    if (error) {
-      console.error('Error fetching applicant:', error)
-      return
+    if (file) {
+      setResumePreviewUrl(URL.createObjectURL(file))
     }
-    return data
   }
 
   const handleSubmit = async () => {
@@ -61,60 +50,104 @@ const Applicantdashboard = ({ applicant, onRefresh }) => {
       // upload resume to storage
       const resumeBucketPath = `${session.user.id}/${resumeFile.name}`
 
+      //upload resume to storage
       const { data: file, error: uploadError } = await supabase.storage
         .from('resume')
         .upload(resumeBucketPath, resumeFile)
+
       if (uploadError) {
         console.log('uploadError: ', uploadError)
         throw uploadError
       }
 
-      // check if applicant exists
-      const applicantExists = await ifApplicantExists()
       //get resume url from storage
-      console.log('pass the upload')
       const resp = await supabase.storage.from('resume').getPublicUrl(resumeBucketPath)
       const resumeUrl = resp.data.publicUrl
-      console.log('resumeUrl', resumeUrl)
-      if (applicantExists) {
-        // update applicant data
-        console.log('update applicant data')
-        const { data, error } = await supabase
-          .from('applicants')
-          .update(
-            {
-              name,
-              phone,
-              email,
-              resume_url: resumeUrl
-            },
-            { upsert: true }
-          )
-          .eq('users_id', session.user.id)
 
-        if (error) {
-          console.error('Error updating applicant:', error)
-          return
-        }
-      } else {
-        // insert applicant data to db
-        const { data, error } = await supabase
-          .from('applicants')
-          .insert({
-            name,
-            phone,
-            email,
-            resume_url: resumeUrl,
-            users_id: session.user.id
-          })
-          .eq('users_id', 'b9210e4c-9f4a-4379-bb04-64c6edce46bb')
-        if (error) {
-          console.error('Error submitting form:', error)
-        }
+      // insert applicant data to db
+      const { data, error } = await supabase.from('applicants').insert({
+        name,
+        phone,
+        email,
+        resume_url: resumeUrl,
+        users_id: session.user.id
+      })
+
+      if (error) {
+        console.error('Error submitting form:', error)
+        throw error
       }
+
       setResumePreviewUrl('')
+      setResumeFile('')
       onRefresh()
     }
+  }
+
+  const handleUpdate = async () => {
+    if (name && phone && email) {
+      const updateData = { name, phone, email }
+
+      if (resumeFile) {
+        // get public resume url from storage
+        const resumeBucketPath = `${session.user.id}/${resumeFile.name}`
+        const resp = await supabase.storage.from('resume').getPublicUrl(resumeBucketPath)
+        const resumeUrl = resp.data.publicUrl
+
+        // get uploaded resume name
+        const uploadedResumeName = decodeURIComponent(applicant.resume_url.split('/').pop())
+
+        // check if resume file is the same as the uploaded one and rewrite it if it is
+        if (resumeFile.name === uploadedResumeName) {
+          const { data: file, error: uploadError } = await supabase.storage
+            .from('resume')
+            .update(resumeBucketPath, resumeFile, { upsert: true })
+          updateData.resume_url = resumeUrl
+          setResumePreviewUrl('')
+          setResumeFile('')
+          console.log('rewrite resume file')
+          if (uploadError) {
+            console.log('uploadError: ', uploadError)
+            throw uploadError
+          }
+        } else {
+          // upload new resume file and delete the old one
+          const { data: file, error: uploadError } = await supabase.storage
+            .from('resume')
+            .upload(resumeBucketPath, resumeFile)
+          console.log('hello')
+          updateData.resume_url = resumeUrl
+          setResumePreviewUrl('')
+          setResumeFile('')
+          if (uploadError) {
+            console.log('uploadError: ', uploadError)
+            throw uploadError
+          }
+
+          // delete old resume file
+          const { data, error: deleteError } = await supabase.storage
+            .from('resume')
+            .remove([`${session.user.id}/${uploadedResumeName}`])
+          console.log('path3', `${session.user.id}/${uploadedResumeName}`)
+          console.log('removehere4: ', data)
+          if (deleteError) {
+            console.log('deleteError: ', deleteError)
+            throw deleteError
+          }
+        }
+      }
+
+      // update applicant data
+      const { data, error } = await supabase
+        .from('applicants')
+        .update(updateData)
+        .eq('users_id', session.user.id)
+      if (error) {
+        console.error('Error updating applicant:', error)
+        throw error
+      }
+    }
+    onRefresh()
   }
 
   if (status === 'loading') {
@@ -123,6 +156,7 @@ const Applicantdashboard = ({ applicant, onRefresh }) => {
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <div className="w-full max-w-md  p-4 sm:p-8 bg-white rounded-lg shadow-md ">
+        {/* application form */}
         <h2 className="text-2xl font-semibold mb-6 text-dark">Application Form</h2>
         <input
           className="w-full p-2 mb-4 border-[3px] border-dark rounded"
@@ -164,43 +198,71 @@ const Applicantdashboard = ({ applicant, onRefresh }) => {
           onChange={resumeHandler}
           className="hidden"
         />
-        <div className="text-dark text-lg font-semibold mt-4">
-          <p className="">Uploaded Resume: </p>
-        </div>
-        {applicant?.resume_url ? (
-          <div className="border-[3px] border-dark rounded p-2 mt-4">
-            <a href={applicant.resume_url} target="_blank" className="flex justify-between">
-              <div>
-                <GrDocumentPdf className="text-xl inline-block mr-1" />
-                {decodeURIComponent(applicant.resume_url.split('/')[9])}
-              </div>
-              <FiDownload className="text-2xl inline-block" />
-            </a>
-          </div>
-        ) : (
-          <div>
-            <p>No resume uploaded</p>
-          </div>
-        )}
 
+        {/* Preview resume */}
         {resumePreviewUrl && (
-          <div className="border-[3px] border-dark rounded p-2 mt-4">
-            <a href={resumePreviewUrl} target="_blank" className="flex justify-between">
-              <div>
-                <GrDocumentPdf className="text-xl inline-block mr-1" />
-                {resumeFile.name}
-              </div>
-
-              <FiDownload className="text-2xl inline-block" />
-            </a>
+          <div className="text-lg font-semibold mt-4">
+            <p className=" text-primary mb-2">Preview Resume: </p>
+            <div className=" border-b-[3px] border-dark rounded py-2">
+              <a
+                href={resumePreviewUrl}
+                target="_blank"
+                className="flex justify-between items-center"
+              >
+                <div className="flex justify-center items-center mr-1 overflow-hidden">
+                  <GrDocumentPdf className="text-3xl mr-3 flex-shrink-0" />
+                  <p className="max-w-[350px] overflow-hidden whitespace-nowrap text-ellipsis">
+                    {resumeFile?.name}
+                  </p>
+                </div>
+                <FiDownload className="text-2xl flex-shrink-0" />
+              </a>
+            </div>
           </div>
         )}
-        <button
-          className="w-full px-4 py-2 font-semibold text-black bg-primary text-white rounded mt-6"
-          onClick={handleSubmit}
-        >
-          Submit
-        </button>
+
+        {/* Uploaded resume */}
+        <div className="text-lg font-semibold mt-4">
+          <p className=" text-primary mb-2">Uploaded Resume: </p>
+          {applicant?.resume_url ? (
+            <div className="border-b-[3px] border-dark rounded py-2 ">
+              <a
+                href={applicant.resume_url}
+                target="_blank"
+                className="flex justify-between items-center hover:underline"
+              >
+                <div className="flex justify-center items-center mr-1 overflow-hidden">
+                  <GrDocumentPdf className="text-3xl mr-3 flex-shrink-0" />
+                  <p className="max-w-[350px] overflow-hidden whitespace-nowrap text-ellipsis ">
+                    {decodeURIComponent(applicant.resume_url.split('/').pop())}
+                  </p>
+                </div>
+                <FiDownload className="text-2xl" />
+              </a>
+            </div>
+          ) : (
+            <div>
+              <p>No resume uploaded.</p>
+            </div>
+          )}
+        </div>
+
+        {/* Submit button */}
+        {applicant ? (
+          <button
+            className="w-full px-4 py-2 font-semibold text-black bg-primary text-white rounded mt-6"
+            onClick={handleUpdate}
+          >
+            Update Profile
+          </button>
+        ) : (
+          <button
+            className="w-full px-4 py-2 font-semibold text-black bg-primary text-white rounded mt-6"
+            onClick={handleSubmit}
+          >
+            Create Profile
+          </button>
+        )}
       </div>
     </div>
   )
