b09c06065e5ad4a1ba90fe429d506ad4fcc5670b
Jimmy Yao <jimmyyaocs@gmail.com>
2023-05-30 19:53:30 +1000

    feat: add log out button to mobile navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Logo from '../../img/logo.png'
 import { useSession, signOut } from 'next-auth/react'
 import { signIn } from 'next-auth/react'
+import { sendError } from 'next/dist/server/api-utils'
 
 function Header() {
   const { data: session } = useSession()
@@ -27,9 +28,11 @@ function Header() {
           <Link className=" text-black  text-lg" href="/">
             Home
           </Link>
-          <Link className=" text-black  text-lg" href={`/dashboard/profile/${session?.user.id}`}>
-            Temporary_Dashboard
-          </Link>
+          {session && (
+            <Link className=" text-black  text-lg" href={`/dashboard/profile/${session?.user.id}`}>
+              Dashboard
+            </Link>
+          )}
 
           <Link className="hidden text-primary font-semibold text-md" href="/about">
             About Us
@@ -52,12 +55,12 @@ function Header() {
             <button
               onClick={() => {
                 signOut()
-                localStorage.clear()
+                // localStorage.clear()
 
                 // set 1 Jan, 1970 expiry for every cookies
-                for (var i = 0; i < Cookies.length; i++)
-                  document.cookie = Cookies[i] + '=;expires=' + new Date(0).toUTCString()
-                showCookies()
+                // for (var i = 0; i < Cookies.length; i++)
+                //   document.cookie = Cookies[i] + '=;expires=' + new Date(0).toUTCString()
+                // showCookies()
               }}
               className="bg-primary h-1/2 w-full rounded-b-xl flex-1 px-3 py-1 text-white hover:bg-dark transition-colors-[2sec]  text-center duration-300"
             >
@@ -69,13 +72,14 @@ function Header() {
           </div>
         </div>
       </div>
+
       {/* Mobile */}
       <div className="sm:hidden">
         <div className=" bg-primary w-screen h-[60px] flex items-center justify-between px-5">
           <Link className="flex gap-3 items-center" href="/">
-            <Image src={Logo} alt="" className="h-[50px] w-[50px] " />
+            <Image src={Logo} alt="" className="h-[50px] w-[50px]" />
             <div className="">
-              <h3 className=" text-2xl text-white font-semibold">Jr-dev</h3>
+              <h3 className="text-2xl text-white font-semibold">Jr-dev</h3>
               <p className="text-white">Code.Sydney</p>
             </div>
           </Link>
@@ -86,22 +90,46 @@ function Header() {
           </div>
         </div>
         <div className={navIsVisible ? 'flex items-end' : 'hidden'}>
-          <div className=" border-[1.5px] shadow-lg border-t-0 border-primary z-10 absolute right-0 bg-white top-[60px] items-center flex flex-col px-5 pt-6 rounded-bl-3xl pb-10 gap-3">
+          <div className="border-[1.5px] shadow-lg border-t-0 border-primary z-10 absolute right-0 bg-white top-[60px]  justify-center items-center flex flex-col px-5 pt-6 rounded-bl-3xl pb-10 gap-3">
+            <div className="px-3 py-1 font-semibold ">
+              <h3>Hey: {session && session.user.name}</h3>
+            </div>
             <Link className=" text-black text-lg" href="/">
               Home
             </Link>
 
+            {session && (
+              <Link className=" text-black text-lg" href={`/dashboard/profile/${session?.user.id}`}>
+                Dashboard
+              </Link>
+            )}
+            {session && (
+              <button
+                className="text-black text-lg"
+                onClick={() => {
+                  signOut()
+                }}
+              >
+                Log out
+              </button>
+            )}
+
             <Link className="hidden text-black text-lg" href="/about">
               About Us
             </Link>
+
             <Link
-              className="border-[2px] shadow-lg my-auto rounded-full px-3 border-primary  text-lg"
+              className={`border-[2px] shadow-lg my-auto rounded-full px-3 border-primary text-lg ${
+                session ? 'hidden' : ''
+              }`}
               href="/signin"
             >
               Sign In
             </Link>
             <Link
-              className="border-[2px] shadow-lg my-auto rounded-full bg-primary text-white border-primary px-3 text-lg"
+              className={`border-[2px] shadow-lg my-auto rounded-full bg-primary text-white border-primary px-3 text-lg ${
+                session ? 'hidden' : ''
+              }`}
               href="/signin"
             >
               Sign Up
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,9 @@ import { SessionProvider } from 'next-auth/react'
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   )
 }
